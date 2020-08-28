57e0260a103ca4ce5f4e5b231db8e7ba887c90e8
unknown <felipe.bordignon@acad.pucrs.br>
2020-08-28 17:28:14 -0300

    before pet clinic

--- Projeto/springapp/src/main/java/curso/dejava/springapp/controllers/BookController.java
+++ Projeto/springapp/src/main/java/curso/dejava/springapp/controllers/BookController.java
@@ -14,13 +14,11 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books ")
+    @RequestMapping("/books")
     public String getBooks(Model model){
         model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
-
-
 
 }
